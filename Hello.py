b7e651b3ca8c0087e399561c9aeb1e8e808531ef
Fabrycio Nakano <90360914+Fabrycio-Nakano@users.noreply.github.com>
2024-04-16 18:35:09 +0000

    new york uber

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+'''# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# :balloon: Welcome to Streamlit! 👋")
 
     st.sidebar.success("Select a demo above.")
 
@@ -49,3 +49,52 @@ def run():
 
 if __name__ == "__main__":
     run()
+'''
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('FABRYCIO LEITE NAKANO ALMADA')
+
+st.subheader('Corridas de Uber em NYC')
+
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+ 
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Carregando dados...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Mostrar Dados Brutos'):
+    st.subheader('Dados Brutos')
+    st.write(data)
+
+
+
+st.subheader('Número de corridas por hora')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    
+st.bar_chart(hist_values)
+
+st.subheader('Mapa de todas as corridas')
+st.map(data)
+
+hour_to_filter = st.slider('Hora do dia', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Mapa de todas as corridas às {hour_to_filter}:00')
+st.map(filtered_data)
